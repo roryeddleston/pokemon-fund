6c97cd8406ceaee1fcfe963b00461027c2aea99b
Rory Eddleston <rory_94@live.co.uk>
2025-10-24 18:25:32 +0100

    adding colours and fonts to global css

---
+++ src/styles/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Cinzel } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+export const cinzel = Cinzel({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  display: "swap",
+});
