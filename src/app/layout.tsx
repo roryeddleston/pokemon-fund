e81a00e989e4f270fd8485a180ee5b44e7666ff8
Rory Eddleston <rory_94@live.co.uk>
2025-10-28 14:42:32 +0000

    creating navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-// app/layout.tsx
 import "./globals.css";
 import { Container } from "@/components/ui/container";
-import Topbar from "@/components/nav/TopbarServer";
+import Topbar from "@/components/layout/Topbar/TopbarServer";
+import Footer from "@/components/layout/Footer/Footer";
 import { inter, cinzel } from "@/styles/fonts";
 
 export const metadata = {
@@ -9,7 +9,11 @@ export const metadata = {
   description: "Strategy-first, vault-secured Pokémon investing.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
@@ -17,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <Topbar />
         <Container className="py-8">{children}</Container>
+        <Footer />
       </body>
     </html>
   );
